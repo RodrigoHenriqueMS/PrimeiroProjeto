c088f7243424039ebfe312c5f6d5850443953a09
Digo <Digo@DIGOPC>
2023-10-25 13:29:11 -0300

    Adicionar arquivos de projeto.

---
+++ PrimeiroProjeto/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PrimeiroProjeto
+{
+    internal class PrimeiroPrograma
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
